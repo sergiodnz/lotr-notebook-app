9c8fa5b41b7947a555de4aa855ebadb3452ce78b
Claudio Romero <romerolrocha@gmail.com>
2020-06-28 15:51:31 -0300

    Create fake routes

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>The Lord of The Rings</h1>
+      <h3>Notebook App</h3>
+    </div>
+  );
+};
+
+export default Home;
