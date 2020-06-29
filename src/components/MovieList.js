1163449733703f5d7e93c50f7b515f2907055205
Claudio Romero <romerolrocha@gmail.com>
2020-06-28 22:52:53 -0300

    Add sorting to movie lists

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Movie from './Movie';
 
+const SORT_BY = {
+  NAME: 'name',
+  ACADEMY_AWARDS: 'academyAwardWins',
+};
+
+const gestSortedMovies = (movies, sortBy) => {
+  switch (sortBy) {
+    case SORT_BY.NAME:
+      return movies.slice().sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+    default:
+      return movies.slice().sort((a, b) => b[sortBy] - a[sortBy]);
+  }
+};
+
 const MovieList = ({ title, movies, onUpdateMovie, onResetMovie }) => {
+  const [sortBy, setSortBy] = useState(SORT_BY.ACADEMY_AWARDS);
+  const sortedMovies = gestSortedMovies(movies, sortBy);
+
   return (
     <div className="movie-list">
       <h3>
         <span>{title}</span>
-        <button className="sort-button">
+        <button
+          className="sort-button"
+          onClick={() => setSortBy(SORT_BY.ACADEMY_AWARDS)}
+        >
           <i className="fa fa-sort-amount-desc"></i>
         </button>
-        <button className="sort-button">
+        <button className="sort-button" onClick={() => setSortBy(SORT_BY.NAME)}>
           <i className="fa fa-sort-alpha-asc"></i>
         </button>
       </h3>
       <ul>
-        {movies.map(movie => (
+        {sortedMovies.map(movie => (
           <li key={movie._id} className="movie-list-item">
             <Movie
               movie={movie}
